2edcd5100e459711e71a01b7adc2151888fee784
timotheekim10 <timotheekim10@gmail.com>
2023-03-03 23:25:04 +0900

    Link Design to Code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlet은 UI 요소를 참조할 수 있게 해줌.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 뷰가 로드될 때 viewDidLoad 내부 코드 블록 실행
+        
+        //WHO          WHAT    VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
